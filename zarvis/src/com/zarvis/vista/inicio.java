bb9ca2ed25aae3e73a4ae9b32ad59d29877c2056
ZARVIS <izaro.garcia.uribe@gmail.com>
2019-09-24 19:32:44 +0200

    Subida inicial

---
+++ zarvis/src/com/zarvis/vista/inicio.java
@@ -0,0 +1,17 @@
+package com.zarvis.vista;
+
+import javax.swing.*;
+
+public class inicio {
+    private JPanel background;
+    private JPanel panelPrincipal;
+    private JPanel PanelSuperior;
+    private JPanel PanelMedio;
+    private JLabel NombreEmpresa;
+    private JTextField textField1;
+    private JTextField textField2;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
