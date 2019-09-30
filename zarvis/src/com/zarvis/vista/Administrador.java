62f214e75616d9909c3ec9b40749647fa8fed4b6
Izaro <izaro.garcia.uribe@gmail.com>
2019-09-30 21:06:32 +0200

    porfa funciona

---
+++ zarvis/src/com/zarvis/vista/Administrador.java
@@ -0,0 +1,14 @@
+package com.zarvis.vista;
+
+import javax.swing.*;
+
+public class Administrador {
+    private JPanel Panel1;
+    private JPanel PanelTitulo;
+    private JPanel PantallaBotones;
+    private JButton empleadoButton;
+    private JButton actividadButton;
+    private JButton clienteButton;
+    private JButton volverButton;
+    private JButton cerrarSesiónButton;
+}
