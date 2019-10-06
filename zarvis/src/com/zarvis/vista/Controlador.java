9f7c3f6f227752e2a132eb67a9c9875522d53d10
isabel <isa.montero92@gmail.com>
2019-10-06 16:44:16 +0200

    MetodosDB4O
    Métodos definidos aunque aun con fallos.

--- zarvis/src/com/zarvis/vista/Controlador.java
+++ zarvis/src/com/zarvis/vista/Controlador.java
@@ -2,16 +2,88 @@ package com.zarvis.vista;
 
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.ext.Db4oException;
+
+import java.util.ArrayList;
 
 public class Controlador {
 
-
-    public static void ConexionBBDD (){
-
-        String BDZAzarvis = "BBDDzarvis.yap";
-        ObjectContainer db= Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(),BDZAzarvis);
+    //DB4O
+    String BDZAzarvis = "BBDDzarvis.yap";
+    ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), BDZAzarvis);
 
 
+    public static void ConexionBBDD() {
+
+
+    }
+
+    //función para guardar datos en la bbdd de db4o
+    public void guardarenBBDD(Object objeto) {
+
+        try {
+
+            db.store(objeto);
+
+        } finally {
+            db.close();
+        }
+    }
+
+    public void borrardelaBBDD(Object objeto) throws Db4oException {
+
+        ObjectSet<Object> result = db.queryByExample(objeto);
+        db.delete(result);
+
+    }
+
+    public void consultaBBDD(Object objeto, String nombre) {
+
+        //Diferentes arrays para devolver
+        ArrayList<Actividad> consActividades = new ArrayList<>();
+        ArrayList<Cliente> consClientes = new ArrayList<>();
+        ArrayList<Empleado> consEmpleados = new ArrayList<>();
+        ArrayList<Sancion> consSanciones = new ArrayList<>();
+        ArrayList<Usuario> consUsuario = new ArrayList<>();
+
+        ObjectSet<Object> result = db.queryByExample(objeto);
+
+        if (result.size() == 0) {
+            System.out.println("No existe");
+        }
+
+        while (result.hasNext()) {
+
+
+            switch (nombre) {
+                case "Actividad":
+
+                    Actividad p = (Actividad) result.next();
+                    consActividades.add(p);
+                    //return consActividades;
+
+                case "Cliente":
+                    Cliente a = (Cliente) result.next();
+                    consClientes.add(a);
+                    //return consClientes;
+                case "Empleado":
+                    Empleado b = (Empleado) result.next();
+                    consEmpleados.add(b);
+
+                case "Sancion":
+                    Sancion c = (Sancion) result.next();
+                    consSanciones.add(c);
+
+                case "Usuario":
+                    Usuario d = (Usuario) result.next();
+                    consUsuario.add(d);
+
+                default:
+            }
+
+
+        }
 
     }
 }
